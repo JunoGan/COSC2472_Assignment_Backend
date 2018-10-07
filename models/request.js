66e791500a1e491035a61ff8a78cfbd016544ef7
Jiaxian Gan <s3582657@student.rmit.edu.au>
2018-10-07 17:59:44 +1100

    tiny change

--- models/request.js
+++ models/request.js
@@ -53,7 +53,7 @@ function validateRequest(request) {
     status: Joi.string().min(1).max(255).required(),
     time: Joi.string().min(1).max(255).required(),
     title: Joi.string().min(1).max(255).required(),
-    uid: Joi.String.request()
+    uid: Joi.String().request()
   };
 
   return Joi.validate(request, schema);
