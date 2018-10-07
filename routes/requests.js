df062701115e0d51711989cd571e0217fdfc615c
Jiaxian Gan <s3582657@student.rmit.edu.au>
2018-10-07 18:19:51 +1100

    change

--- routes/requests.js
+++ routes/requests.js
@@ -14,7 +14,6 @@ router.post('/', async (req, res) => {
     status: req.body.status,
     time: req.body.time,
     title: req.body.title,
-    uid: req.body.uid
   });
   await request.save();
   
