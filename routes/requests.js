a2e1e83f203866c480b7572130ad736a7249011f
Jiaxian Gan <s3582657@student.rmit.edu.au>
2018-10-07 17:55:32 +1100

    commit

--- routes/requests.js
+++ routes/requests.js
@@ -14,6 +14,7 @@ router.post('/', async (req, res) => {
     status: req.body.status,
     time: req.body.time,
     title: req.body.title,
+    uid: req.body.uid
   });
   await request.save();
   
